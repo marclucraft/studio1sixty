7ea4518736422bbf5d72f4968fadb6fb4a5ae02e
Marc Lucraft <marclucraft@gmail.com>
2019-06-23 07:42:54 +0100

    Add ken burns slider

--- js/main.js
+++ js/main.js
@@ -338,4 +338,32 @@ $(document).ready(function() {
         $('#mc_embed_signup').find('form').ajaxChimp();
     });
 
+    var new_effect;
+var old_effect;
+
+function random_effect() {
+	return Math.floor(Math.random() * 6) + 1;
+}
+
+$(function() {
+  
+  new_effect = random_effect();
+  $('.cycle-slideshow img:first-child').addClass('scale');
+  $('.cycle-slideshow img:first-child').addClass('fx' + new_effect);
+  
+	$('.cycle-slideshow').on('cycle-before', function(event, optionHash, outgoingSlideEl, incomingSlideEl, forwardFlag) {
+		old_effect = new_effect;
+		new_effect = random_effect();
+		$(incomingSlideEl).addClass('scale');
+		$(incomingSlideEl).addClass('fx' + new_effect);
+	});
+	
+	$('.cycle-slideshow').on('cycle-after', function(event, optionHash, outgoingSlideEl, incomingSlideEl, forwardFlag) {
+		$(outgoingSlideEl).removeClass('scale');
+		$(outgoingSlideEl).removeClass('fx' + old_effect);
+	});
 });
+
+
+});
+
